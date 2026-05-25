de6eb5ff74911351be26243b7a64b386f2cc5c80
Raman Kumar <kraman938@gmail.com>
2026-05-25 20:09:00 +0530

    Initializes payment adapter service

---
+++ payment-adapter-service/src/main/java/txnflow/paymentadapterservice/PaymentAdapterServiceApplication.java
@@ -0,0 +1,13 @@
+package txnflow.paymentadapterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentAdapterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentAdapterServiceApplication.class, args);
+	}
+
+}
---
+++ payment-adapter-service/src/test/java/txnflow/paymentadapterservice/PaymentAdapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package txnflow.paymentadapterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentAdapterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
